4f15ad60df3549509f9f2471c324781ecc20b0ee
Cláudio José Antunes Mazur <claudioivp@gmail.com>
2023-04-23 11:43:58 -0300

    create Chat function implemented

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,10 +1,15 @@
+import { Chat } from "./../../../../node_modules/.prisma/client/index.d";
+import { prisma } from "@/prisma/prisma";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const prisma = new PrismaClient();
-  prisma.$connect();
-
   const body = await request.json();
-  return NextResponse.json({ body });
+  const chatCreated = await prisma.chat.create({
+    data: {
+      mensagem: body.mensagem,
+    },
+  });
+
+  return NextResponse.json(chatCreated);
 }

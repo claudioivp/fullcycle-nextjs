7335bd30605041f1b4777d34253f4bada55f5114
Cláudio José Antunes Mazur <claudioivp@gmail.com>
2023-04-23 11:15:29 -0300

    prisma migrate for api/chats created

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,6 +1,10 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+  const prisma = new PrismaClient();
+  prisma.$connect();
+
   const body = await request.json();
   return NextResponse.json({ body });
 }
